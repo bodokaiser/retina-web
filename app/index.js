92585a082a2d65be14b34f0dd70411124a39470b
Bodo Kaiser <i@bodokaiser.io>
2015-04-26 19:16:29 +0200

    added duo-babel for es6 support

--- app/index.js
+++ app/index.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+import drop  from 'component/drop';
+import query from 'component/query';
+
+var el = query('.placeholder');
+
+drop(el, function(el) {
+  console.log(el);
+});
